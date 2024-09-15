0f155e42534e6f0c1972a57e76a14ad53598dc73
Nazrulsazu <73848990+Nazrulsazu@users.noreply.github.com>
2024-09-16 01:07:15 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using UserManagementApp.Data;
+using UserManagementApp.Models;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Configure Entity Framework Core with SQL Server
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add Identity services
+builder.Services.AddDefaultIdentity<ApplicationUser>(
+    options => {
+        options.SignIn.RequireConfirmedAccount = true;
+        
+    })
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
+app.Run();
